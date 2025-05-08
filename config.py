38bf080a93746603fc4d652b303738be55742e69
TheHummel <jannisalsbach@gmail.com>
2025-05-08 18:01:52 +0200

    change paths and config

--- config.py
+++ config.py
@@ -14,11 +14,12 @@ PIN_MEMORY = True
 
 # Paths
 current_dir = os.path.dirname(os.path.abspath(__file__))
+OUTPUT_DIR = os.getenv("OUTPUT_DIR")
 data_dir = "/cluster/courses/cil/monocular_depth/data"
 train_dir = os.path.join(data_dir, "train/")
 test_dir = os.path.join(data_dir, "test/")
 train_list_file = os.path.join(data_dir, "train_list.txt")
 test_list_file = os.path.join(data_dir, "test_list.txt")
-output_dir = os.path.join(current_dir, "data/monocular_depth_output")
+output_dir = OUTPUT_DIR
 results_dir = os.path.join(output_dir, "results")
 predictions_dir = os.path.join(output_dir, "predictions")
--- slurm_scripts/run_main.sh
+++ slurm_scripts/run_main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=not_that_deep
-#SBATCH --account=cil
-#SBATCH --output=../data/monocular_depth_output/logs/%j.out
-#SBATCH --error=../data/monocular_depth_output/logs/%j.err
-#SBATCH --time=01:00:00
+#SBATCH --account=cil_jobs
+#SBATCH --output=../output/logs/%j.out
+#SBATCH --error=../output/logs/%j.err
+#SBATCH --time=02:00:00
 #SBATCH --partition=gpu
 #SBATCH --mem=16G
 #SBATCH --ntasks=1
@@ -11,8 +11,11 @@
 
 set -e
 ROOT_DIR="../"
-mkdir -p ${ROOT_DIR}/data/monocular_depth_output/{predictions,results,logs}
+MODEL_OUTPUT_DIR="intel_dpt_large"
+OUTPUT_DIR="${ROOT_DIR}/output/${MODEL_OUTPUT_DIR}"
+mkdir -p ${ROOT_DIR}/output/${MODEL_OUTPUT_DIR}/{predictions,results}
 module load cuda/12.6.0
 source ~/.bashrc
 conda activate /cluster/courses/cil/envs/monocular_depth/
+export OUTPUT_DIR
 python ${ROOT_DIR}/main.py
