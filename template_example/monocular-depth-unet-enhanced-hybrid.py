01c6611fd82318c026e7de0b9329ed5bdfa25f24
jonas10425 <jonas.vogt@bluewin.ch>
2025-05-28 17:44:32 +0200

    Update monocular-depth-unet-enhanced-hybrid.py
    10 epochen

--- template_example/monocular-depth-unet-enhanced-hybrid.py
+++ template_example/monocular-depth-unet-enhanced-hybrid.py
@@ -33,7 +33,7 @@ print(f"output_dir: {output_dir}")
 BATCH_SIZE = 4
 LEARNING_RATE = 1e-5
 WEIGHT_DECAY = 1e-6
-NUM_EPOCHS = 20
+NUM_EPOCHS = 10
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 INPUT_SIZE = (426, 560)
 NUM_WORKERS = 2
